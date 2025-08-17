a16de1141233b648bedb45fbcf981d18b0bab3be
roggervalf <rogger.valvrd@gmail.com>
2025-08-17 13:24:11 -0600

    feat: only js

---
+++ test.js
@@ -0,0 +1,159 @@
+const { isMatchWith, isString } = require('lodash');
+const micromatch = require('micromatch');
+
+console.log(
+  isMatchWith(
+    { message: 'fix: hola ' },
+    { message: '!**\\[python\\]**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola )' },
+    { message: '!**\\[python\\]**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola [python]' },
+    { message: '!**\\[python\\]**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola [d python] ' },
+    { message: '!**\\[python\\]**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'feat: only py [python]' },
+    { message: '!**\\[python\\]**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+
+console.log('gg');
+
+console.log(
+  isMatchWith(
+    { message: 'fix: hola ' },
+    { message: '!**\\(python\\)**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola )' },
+    { message: '!**\\(python\\)**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola (python)' },
+    { message: '!**\\(python\\)**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola (d python) ' },
+    { message: '!**\\(python\\)**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'feat: only py [python]' },
+    { message: '!**\\(python\\)**' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+
+console.log('gg2');
+
+console.log(
+  isMatchWith(
+    { message: 'fix: hola ' },
+    { message: '!(*\\(python\\)*|*\\[python\\]*)' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola )' },
+    { message: '!(*\\(python\\)*|*\\[python\\]*)' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola (python) ' },
+    { message: '!(*\\(python\\)*|*\\[python\\]*)' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'fix: hola (d python) ' },
+    { message: '!(*\\(python\\)*|*\\[python\\]*)' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
+console.log(
+  isMatchWith(
+    { message: 'feat: only py [python] ' },
+    { message: '!(*\\(python\\)*|*\\[python\\]*)' },
+    (object, src) =>
+      isString(src) && isString(object)
+        ? micromatch.isMatch(object, src)
+        : undefined,
+  ),
+);
